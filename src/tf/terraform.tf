6a4d281c2f639f63f15626263877ee979b1d61ea
devlau11 <dev_lau11@a.jlv6.com>
2024-10-23 19:54:51 +0200

    Added default tags for tfc managed resources

--- src/tf/terraform.tf
+++ src/tf/terraform.tf
@@ -9,4 +9,11 @@ terraform {
 
 provider "aws" {
   region = var.AWS_PROD_REGION
+  default_tags {
+    tags = {
+      application = "jlv6.com"
+      environment = "production"
+      managed-by  = "gha"
+    }
+  }
 }
