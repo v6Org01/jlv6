e912b4bab34ea3fff2fb0206ab46bf4178224f30
elaurensx <laurens@jlv6.com>
2024-11-13 10:23:12 +0100

    Deleted tf production files

--- src/tf/production/variables.tf
+++
@@ -1,6 +0,0 @@
-# Declaring blank variables
-# Value stored in TFC.
-variable "AWS_PROD_REGION" {}
-variable "AWS_PROD_S3_BUCKET_01" {}
-variable "AWS_PROD_S3_BUCKET_02" {}
-variable "DOMAIN_NAME" {}
