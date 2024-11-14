2e176d3304bd442e44955f143037a00c46136c8c
elaurensx <laurens@jlv6.com>
2024-11-14 14:39:52 +0100

    Created new output definitions for staging

--- src/tf/shared/outputs.tf
+++ src/tf/shared/outputs.tf
@@ -1,4 +1,5 @@
-#ACM
+## ACM ##
+
 output "aws_acm_certificate_cert01_arn" {
   description = "The ARN of the ACM Certificate."
   value       = aws_acm_certificate.cert01.arn
--- src/tf/staging/outputs.tf
+++ src/tf/staging/outputs.tf
@@ -1,5 +1,13 @@
-#S3
-output "module_s3_bucket_01_s3_bucket_id" {
-  description = "The name of S3 bucket 01"
-  value = try(module.s3_bucket_01.s3_bucket_id, "")
+## S3 ##
+
+output "module_s3_bucket_01_s3_bucket_arn" {
+  description = "The arn of S3 bucket 01"
+  value = try(module.s3_bucket_01.s3_bucket_arn, "")
+}
+
+## CLOUDFRONT ##
+
+output "module_cf_distribution_01_cloudfront_distribution_arn" {
+  description = "The arn of Cloudfront distribution 01"
+  value = try(module.cf_distribution_01.cloudfront_distribution_arn, "")
 }
