6ffc5b7c9c8a515947b65b9c59d1b9fce927fda6
elaurensx <laurens@jlv6.com>
2024-11-14 12:22:12 +0100

    Deleted acm cert definition from output.tf

--- src/tf/staging/outputs.tf
+++ src/tf/staging/outputs.tf
@@ -3,9 +3,3 @@ output "module_s3_bucket_01_s3_bucket_id" {
   description = "The name of S3 bucket 01"
   value = try(module.s3_bucket_01.s3_bucket_id, "")
 }
-
-#ACM
-output "aws_acm_certificate_cert01_arn" {
-  description = "The ARN of the ACM Certificate."
-  value       = try(aws_acm_certificate.cert01.arn, "")
-}
