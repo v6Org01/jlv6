0617336e91ea393003729a1dc1df14d1c6e46e68
elaurensx <laurens@jlv6.com>
2024-11-19 20:16:31 +0100

    Fixed typo in shared/outputs.tf

--- src/tf/shared/outputs.tf
+++ src/tf/shared/outputs.tf
@@ -2,7 +2,7 @@
 
 output "aws_acm_certificate_cert01_arn" {
   description = "The ARN of the ACM Certificate."
-  value       = try(aws_acm_certificate.cert01.arntry, "")
+  value       = try(aws_acm_certificate.cert01.arn, "")
 }
 
 ## CLOUDFRONT ##
