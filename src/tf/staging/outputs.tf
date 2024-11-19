b0322b764261ca1272673d9255ea91ad48256ab1
elaurensx <laurens@jlv6.com>
2024-11-19 09:50:29 +0100

    Added job update-cloudfare-dns to deployStaging2AWS

--- src/tf/staging/outputs.tf
+++ src/tf/staging/outputs.tf
@@ -11,3 +11,8 @@ output "module_cf_distribution_01_cloudfront_distribution_arn" {
   description = "The arn of Cloudfront distribution 01"
   value = try(module.cf_distribution_01.cloudfront_distribution_arn, "")
 }
+
+output "module_cf_distribution_01_cloudfront_distribution_domain_name" {
+  description = "The domain name corresponding to Cloudfront distribution 01"
+  value = try(module.cf_distribution_01.cloudfront_distribution_domain_name, "")
+}
