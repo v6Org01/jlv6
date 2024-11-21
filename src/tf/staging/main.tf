ac7acd881ec6d8e117b551ac2de43ff8d2e51984
elaurensx <laurens@jlv6.com>
2024-11-21 10:06:02 +0100

    Fixed path in cloudfront.s3origin.origin_path in staging/main.tf

--- src/tf/staging/main.tf
+++ src/tf/staging/main.tf
@@ -123,7 +123,7 @@ module "cf_distribution_01" {
     failoverS3 = {
       domain_name           = module.s3_bucket_01.s3_bucket_bucket_regional_domain_name
       origin_access_control = "s3_oac_01"
-      origin_path           = "/${var.VERSION}/"
+      origin_path           = "/${var.VERSION}"
       custom_header = [
         {
           name  = "X-Deployment-Location"
