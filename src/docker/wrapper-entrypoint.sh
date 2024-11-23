ca203f6301f446148ae753c8a2c38769e152f573
elaurensx <laurens@jlv6.com>
2024-11-23 15:48:02 +0100

    Updated wrapper-entrypoint.sh script

--- src/docker/wrapper-entrypoint.sh
+++ src/docker/wrapper-entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -x
+
 # Check if BASE_URL is set
 if [ -z "$BASE_URL" ]; then
   echo "Error: BASE_URL environment variable is not set."
@@ -14,5 +16,10 @@ find /public -type f -exec sed -i "s|{{BASE_URL}}|$BASE_URL|g" {} +
 # Log completion
 echo "Base URL replacement completed."
 
-# Start SWS image entrypoint.sh
+# If no arguments are passed to this wrapper script, set default to static-web-server
+if [ -z "$1" ]; then
+    set -- static-web-server
+fi
+
+# Forward all arguments to the original entrypoint script
 exec /entrypoint.sh "$@"
