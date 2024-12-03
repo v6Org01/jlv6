4f1d829bad6c604627f7426aeae8be1f74cea456
elaurensx <laurens@jlv6.com>
2024-12-03 18:23:58 +0100

    Added cloufront cache invalidation step to deployStaging2AWS and deployProduction2AWS workflows

--- src/tf/production/outputs.tf
+++ src/tf/production/outputs.tf
@@ -4,7 +4,6 @@ output "module_s3_bucket_01_s3_bucket_arn" {
   description = "The arn of S3 bucket 01"
   value = try(module.s3_bucket_01.s3_bucket_arn, "")
 }
-
 output "module_s3_bucket_01_s3_bucket_id" {
   description = "The name of S3 bucket 01"
   value = try(module.s3_bucket_01.s3_bucket_id, "")
@@ -16,8 +15,11 @@ output "module_cf_distribution_01_cloudfront_distribution_arn" {
   description = "The arn of Cloudfront distribution 01"
   value = try(module.cf_distribution_01.cloudfront_distribution_arn, "")
 }
-
 output "module_cf_distribution_01_cloudfront_distribution_domain_name" {
   description = "The domain name corresponding to Cloudfront distribution 01"
   value = try(module.cf_distribution_01.cloudfront_distribution_domain_name, "")
+}
+output "module_cf_distribution_01_cloudfront_distribution_id" {
+  description = "The name of Cloudfront distribution 01"
+  value = try(module.cf_distribution_01.cloudfront_distribution_id, "")
 }
--- src/tf/staging/outputs.tf
+++ src/tf/staging/outputs.tf
@@ -11,3 +11,7 @@ output "module_cf_distribution_01_cloudfront_distribution_domain_name" {
   description = "The domain name corresponding to Cloudfront distribution 01"
   value = try(module.cf_distribution_01.cloudfront_distribution_domain_name, "")
 }
+output "module_cf_distribution_01_cloudfront_distribution_id" {
+  description = "The name of Cloudfront distribution 01"
+  value = try(module.cf_distribution_01.cloudfront_distribution_id, "")
+}
