d7bd0e917c1567a7c0b9c9ab1a9b750ae3292430
elaurensx <laurens@jlv6.com>
2025-03-13 20:54:23 +0100

    Removed var from tf/prod

--- src/tf/production/variables.tf
+++ src/tf/production/variables.tf
@@ -2,7 +2,6 @@
 # Value stored in TFC.
 variable "ADN" {}
 variable "AWS_CF_ORIGIN_JLV6_URI" {}
-variable "AWS_S3_BUCKET_01" {}
 variable "WWW_URI" {}
 variable "TF_ORG" {}
 variable "TF_WORKSPACE_SHARED" {}
